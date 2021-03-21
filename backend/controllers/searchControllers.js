8590c353591f737b533bcc7b157d2c511f7ea83c
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2021-03-21 14:09:59 -0400

    create index algorithm

--- backend/controllers/searchControllers.js
+++ backend/controllers/searchControllers.js
@@ -1,21 +1,40 @@
 const axios = require('axios');
+const search = require('../searchData');
 
 //@description fetch Google results
 //@route GET /api/search/
 //@PUBLIC
 
 const googleSearch = async (req, res) => {
-  const { keyword } = req.query;
+  // const { keyword } = req.query;
 
-  const url = `https://app.zenserp.com/api/v2/search?apikey=${process.env.ZENSERP_KEY}&q=${keyword}`;
+  // const url = `https://app.zenserp.com/api/v2/search?apikey=${process.env.ZENSERP_KEY}&q=${keyword}`;
 
-  let response;
-  try {
-    response = await axios.get(url);
-  } catch (error) {
-    console.error(error);
+  // let response;
+  // try {
+  //   response = await axios.get(url);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  // let dataString = JSON.stringify(search);
+  // dataString = dataString.split('"');
+
+  let words = search.split(' ');
+
+  let index = {};
+
+  for (let i = 0; i < words.length; i++) {
+    if (index[words[i]]) {
+      index[words[i]]++;
+    } else {
+      index[words[i]] = 1;
+    }
   }
-  res.json({ data: response.data });
+
+  console.log(index);
+
+  res.json({ data: words });
 };
 
 module.exports = { googleSearch };
---
+++ backend/searchData.js
@@ -0,0 +1,268 @@
+// const data = {
+//   query: {
+//     apikey: '8f962690-8996-11eb-86f9-f112784eea33',
+//     q: 'brazil',
+//     url:
+//       'https://www.google.com/search?q=brazil&oq=brazil&hl=en&gl=us&sourceid=chrome&ie=UTF-8',
+//   },
+//   knowledge_graph: [
+//     {
+//       image:
+//         'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//       title: 'Brazil',
+//       subtitle: 'Country in South America',
+//       description:
+//         "DescriptionBrazil, officially the Federative Republic of Brazil, is the largest country in both South America and Latin America. At 8.5 million square kilometers and with over 211 million people, Brazil is the world's fifth-largest country by area and the sixth most populous. Wikipedia",
+//       capital: 'Brasilia',
+//       location_statistical_region_population: '211 million (2019)',
+//       president: 'Jair Bolsonaro',
+//       currency: 'Brazilian real',
+//       language: 'Portuguese',
+//       'okra/answer_panel/_restaurants':
+//         'RestaurantsSearch for: brazil restaurants',
+//       'okra/answer_panel/_geography': 'GeographySearch for: brazil geography',
+//       'okra/answer_panel/_traditions':
+//         'TraditionsSearch for: brazil traditions',
+//       people_also_search_for: [
+//         {
+//           name: 'Mexico',
+//           link:
+//             '/search?hl=en&gl=us&q=Mexico&stick=H4sIAAAAAAAAAONgFuLQz9U3MDRNK1LiBLGSLA3ii7T4nPNzc_PzgjNTUssTK4sXsbL5plZkJufvYGUEABzpDVkzAAAA&sa=X&ved=2ahUKEwiM_Jbs3b_vAhVN8OAKHXwSA5QQxA0wN3oECFIQBQ',
+//           image:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//         },
+//         {
+//           name: 'United States',
+//           link:
+//             '/search?hl=en&gl=us&q=United+States&stick=H4sIAAAAAAAAAONgFuLQz9U3MDRNK1LiBLEsk83LDbT4nPNzc_PzgjNTUssTK4sXsfKG5mWWpKYoBJcklqQW72BlBABKl51vOgAAAA&sa=X&ved=2ahUKEwiM_Jbs3b_vAhVN8OAKHXwSA5QQxA0wN3oECFIQBw',
+//           image:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//         },
+//         {
+//           name: 'China',
+//           link:
+//             '/search?hl=en&gl=us&q=China&stick=H4sIAAAAAAAAAONgFuLQz9U3MDRNK1LiBLFSDEzLjbX4nPNzc_PzgjNTUssTK4sXsbI6Z2TmJe5gZQQAanOPGzIAAAA&sa=X&ved=2ahUKEwiM_Jbs3b_vAhVN8OAKHXwSA5QQxA0wN3oECFIQCQ',
+//           image:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//         },
+//         {
+//           name: 'Argentina',
+//           link:
+//             '/search?hl=en&gl=us&q=Argentina&stick=H4sIAAAAAAAAAONgFuLQz9U3MDRNK1JiB7Gy0lO0-Jzzc3Pz84IzU1LLEyuLF7FyOhalp-aVZOYl7mBlBAAk3xTPNAAAAA&sa=X&ved=2ahUKEwiM_Jbs3b_vAhVN8OAKHXwSA5QQxA0wN3oECFIQCw',
+//           image:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//         },
+//         {
+//           name: 'Canada',
+//           link:
+//             '/search?hl=en&gl=us&q=Canada&stick=H4sIAAAAAAAAAONgFuLQz9U3MDRNK1LiBLFSDMwM0rX4nPNzc_PzgjNTUssTK4sXsbI5J-YlpiTuYGUEAHiSeuszAAAA&sa=X&ved=2ahUKEwiM_Jbs3b_vAhVN8OAKHXwSA5QQxA0wN3oECFIQDQ',
+//           image:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//         },
+//       ],
+//     },
+//   ],
+//   organic: [
+//     {
+//       position: 1,
+//       isCarousel: true,
+//       isVertical: false,
+//       news: [
+//         {
+//           title:
+//             'No vaccines, no leadership, no end in sight. How Brazil became a global threat',
+//           url:
+//             'https://www.cnn.com/2021/03/20/americas/brazil-coronavirus-crisis-vaccine-distribution-shortfalls-latam-intl/index.html',
+//           parsed: {
+//             text:
+//               'No vaccines, no leadership, no end in sight. How Brazil became a global threat',
+//             source: null,
+//             date: '2 hours ago',
+//           },
+//         },
+//         {
+//           title:
+//             'Bolsonaro asks court to reverse Brazil’s COVID-19 restrictions',
+//           url:
+//             'https://www.aljazeera.com/news/2021/3/20/bolsonaro-asks-supreme-court-to-reverse-covid-19-restrictions',
+//           parsed: {
+//             text:
+//               'Bolsonaro asks court to reverse Brazil’s COVID-19 restrictions',
+//             source: 'Aljazeera.com',
+//             date: '13 hours ago',
+//           },
+//         },
+//         {
+//           title:
+//             'Apple Fined $2 Million in Brazil for Selling iPhones Without Chargers',
+//           url:
+//             'https://www.macrumors.com/2021/03/20/apple-brazil-fine-iphone-charger/',
+//           parsed: {
+//             text:
+//               'Apple Fined $2 Million in Brazil for Selling iPhones Without Chargers',
+//             source: null,
+//             date: '5 hours ago',
+//           },
+//         },
+//       ],
+//     },
+//     {
+//       position: 2,
+//       title: 'Brazil - Wikipedia',
+//       url: 'https://en.wikipedia.org/wiki/Brazil',
+//       destination: 'en.wikipedia.org › wiki › Brazil',
+//       description:
+//         'Brazil (Portuguese: Brasil; Brazilian Portuguese: [bɾaˈziw]), officially the Federative Republic of Brazil (Portuguese: República Federativa do Brasil), is the largest country in both South America and Latin America.',
+//       isAmp: false,
+//     },
+//     {
+//       position: 3,
+//       questions: [
+//         {
+//           question: 'Is it dangerous to go to Brazil?',
+//         },
+//         {
+//           question: 'What is Brazil famous for?',
+//         },
+//         {
+//           question: 'Why is Brazil so dangerous?',
+//         },
+//         {
+//           question: 'Is Brazil rich or poor?',
+//         },
+//       ],
+//     },
+//     {
+//       position: 4,
+//       title: 'Brazil (1985 film) - Wikipedia',
+//       url: 'https://en.wikipedia.org/wiki/Brazil_(1985_film)',
+//       destination: 'en.wikipedia.org › wiki › Brazil_(1985_film)',
+//       description:
+//         'Brazil is a 1985 dystopian science fiction film directed by Terry Gilliam and written by Gilliam, Charles McKeown, and Tom Stoppard. The film stars Jonathan ...',
+//       isAmp: false,
+//     },
+//     {
+//       position: 5,
+//       title: 'Brazil | History, Map, Culture, Population, & Facts | Britannica',
+//       url: 'https://www.britannica.com/place/Brazil',
+//       destination: 'www.britannica.com › ... › Countries of the World',
+//       description:
+//         "Brazil, country of South America that occupies half the continent's landmass. It is the fifth largest and fifth most-populous country in the world.",
+//       isAmp: false,
+//       date: '6 days ago',
+//     },
+//     {
+//       position: 6,
+//       title: 'Brazil travel | South America - Lonely Planet',
+//       url: 'https://www.lonelyplanet.com/brazil',
+//       destination: 'www.lonelyplanet.com › brazil',
+//       description:
+//         "One of the world's most captivating places, Brazil is a country of powdery white-sand beaches, verdant rainforests and wild, rhythm-filled metropolises. Brazil's ...",
+//       isAmp: false,
+//     },
+//     {
+//       position: 7,
+//       title: 'Brazil | World | The Guardian',
+//       url: 'https://www.theguardian.com/world/brazil',
+//       destination: 'www.theguardian.com › world › brazil',
+//       description:
+//         "Brazil · Bolsonaro uses Brazil's dictatorship-era law as protesters depict him as a Nazi · Brazil stares into the abyss as Covid intensive care units fill up everywhere.",
+//       isAmp: false,
+//     },
+//     {
+//       position: 8,
+//       title: "Visit Brasil | Brazil's official tourism website",
+//       url: 'https://www.visitbrasil.com/',
+//       destination: 'www.visitbrasil.com',
+//       description:
+//         "Going to Brazil? Find everything about cities in brazil, destinations and brazilian's beauty. All official information you need to plan your trip to Brazil.",
+//       isAmp: false,
+//     },
+//     {
+//       position: 9,
+//       title: 'Brazil | Reuters.com',
+//       url: 'https://www.reuters.com/places/brazil',
+//       destination: 'www.reuters.com › places › brazil',
+//       description:
+//         "Brazil's government has been in talks since March 13 about potentially importing excess COVID-19 vaccines from the United States, the Brazilian Foreign ...",
+//       isAmp: false,
+//     },
+//     {
+//       position: 10,
+//       title:
+//         'Brazil coronavirus: No vaccines, no leadership, no end in sight ...',
+//       url:
+//         'https://www.cnn.com/2021/03/20/americas/brazil-coronavirus-crisis-vaccine-distribution-shortfalls-latam-intl/index.html',
+//       destination: 'www.cnn.com › 2021/03/20 › americas › brazil-coronavi...',
+//       description:
+//         'Rio de Janeiro, Brazil (CNN) The temperature read 95 degrees Fahrenheit on Tuesday, but the humidity made it feel worse. Amid the stifling ...',
+//       isAmp: false,
+//       date: '3 hours ago',
+//     },
+//     {
+//       position: 11,
+//       videos: [
+//         {
+//           title:
+//             "Inside Brazil's Fight Against P.1, a Fast-Spreading Covid-19 ...",
+//           url:
+//             'https://www.wsj.com/video/inside-brazils-fight-against-p1-a-fast-spreading-covid-19-variant/0D5D73CA-BC74-4B4C-B04B-1D62E03D3847.html',
+//           thumbnail:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//           parsed: {
+//             source: 'Wall Street Journal',
+//             date: '1 day ago',
+//             channel: null,
+//           },
+//         },
+//         {
+//           title:
+//             'Massive Surge Coronavirus Cases In Brazil, Health Services ...',
+//           url: 'https://www.youtube.com/watch?v=igXFdBRQDZI',
+//           thumbnail:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//           parsed: {
+//             source: 'YouTube',
+//             date: '1 day ago',
+//             channel: 'India Today',
+//           },
+//         },
+//         {
+//           title:
+//             "Inside Brazil's Fight Against P.1, a Fast-Spreading Covid-19 ...",
+//           url: 'https://www.youtube.com/watch?v=WeQ4OHXyTTs',
+//           thumbnail:
+//             'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+//           parsed: {
+//             source: 'YouTube',
+//             date: '4 hours ago',
+//             channel: 'Wall Street Journal',
+//           },
+//         },
+//       ],
+//     },
+//   ],
+//   related_searches: [],
+//   number_of_results: 1670000000,
+//   pagination: {
+//     current_page: 1,
+//     next_page: 'https://app.zenserp.com/api/v2/search?q=brazil&start=10',
+//     previous_page: null,
+//     pages: {
+//       2: 'https://app.zenserp.com/api/v2/search?q=brazil&start=10',
+//       3: 'https://app.zenserp.com/api/v2/search?q=brazil&start=20',
+//       4: 'https://app.zenserp.com/api/v2/search?q=brazil&start=30',
+//       5: 'https://app.zenserp.com/api/v2/search?q=brazil&start=40',
+//       6: 'https://app.zenserp.com/api/v2/search?q=brazil&start=50',
+//       7: 'https://app.zenserp.com/api/v2/search?q=brazil&start=60',
+//       8: 'https://app.zenserp.com/api/v2/search?q=brazil&start=70',
+//       9: 'https://app.zenserp.com/api/v2/search?q=brazil&start=80',
+//       10: 'https://app.zenserp.com/api/v2/search?q=brazil&start=90',
+//     },
+//   },
+// };
+
+const data =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+module.exports = data;
